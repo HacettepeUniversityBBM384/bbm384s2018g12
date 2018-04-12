aab0268e4164c34c0e1dfe1a85a2da89272580f0
Sefa Ozdogan <sefaozdogan@gmail.com>
2018-04-12 15:14:54 +0300

    Services added

---
+++ src/main/java/com/sport/service/UserService.java
@@ -0,0 +1,29 @@
+package com.sport.service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import com.sport.domain.User;
+
+
+
+public interface UserService {
+	
+	List<User> findByType(String type);
+	
+	Optional<User> findById(Long id);
+	 
+	void saveUser(User usr);
+	void updateUser(User usr);
+	 
+	void deleteUserById(Long id);
+	 
+	void deleteAllUsers();
+	 
+	List<User> findAllUsers();
+	 
+	boolean isUserExist(User usr);
+	
+
+}
